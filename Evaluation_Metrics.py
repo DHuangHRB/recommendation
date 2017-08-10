9725ce9f2be35ad728e2ab700212313144e7661a
DHuangHRB <8607927+DHuangHRB@users.noreply.github.com>
2017-08-09 23:34:07 -0400

    Update Evaluation_Metrics.py

--- Evaluation_Metrics.py
+++ Evaluation_Metrics.py
@@ -1,6 +1,6 @@
 import math
 
-def Evaluation(Recommend, train, test, K, N):
+def Evaluation(Recommend, train, test, W, K, N):
     hit = 0
     all_tu = 0
     all_ru = 0
@@ -20,7 +20,7 @@ def Evaluation(Recommend, train, test, K, N):
             all_items.add(item)
         
         tu = test[user].keys()
-        rank = Recommend(user, train, K, N)
+        rank = Recommend(user, train, W, K, N)
         for item, pui in rank.items():
             recommmend_items.add(item)
             ret_pop += math.log(1 + item_popularity[item])
@@ -72,43 +72,43 @@ def LFM_Evaluation(Recommend, train, test, p, q, N):
         
     return [hit / all_ru, hit / all_tu, len(recommmend_items) / len(all_items), ret_pop]
 
-def Recall(Recommend, train, test, K, N):
+def Recall(Recommend, train, test, W, K, N):
     hit = 0
     all = 0
     for user in train.keys():
         tu = test[user].keys()
         n_recommend = len(test[user])
-        rank = Recommend(user, train, K, N)
+        rank = Recommend(user, train, W, K, N)
         for item, pui in rank.items():
             if item in tu:
                 hit += 1
         all += len(tu)
     return hit / all
 
-def Precision(Recommend, train, test, K, N):
+def Precision(Recommend, train, test, W, K, N):
     hit = 0 
     all = 0
     for user in train.keys():
         tu = test[user].keys()
-        rank = Recommend(user, train, K, N)
+        rank = Recommend(user, train, W, K, N)
         for item, pui in rank.items():
             if item in tu:
                 hit += 1
         all += len(rank)
     return hit / all
 
-def Coverage(Recommend, train, test, K, N):
+def Coverage(Recommend, train, test, W, K, N):
     recommmend_items = set()
     all_items = set()
     for user in train.keys():
         for item in train[user].keys():
             all_items.add(item)
-        rank = Recommend(user, train, K, N)
+        rank = Recommend(user, train, W, K, N)
         for item, pui in rank.items():
             recommmend_items.add(item)
     return len(recommmend_items) / len(all_items)
 
-def Popularity(Recommend, train, test, K, N):
+def Popularity(Recommend, train, test, W, K, N):
     item_popularity = dict()
     for user, items in train.items():
         for item in items.keys():
@@ -118,7 +118,7 @@ def Popularity(Recommend, train, test, K, N):
     ret_pop = 0
     n_pop = 0
     for user in train.keys():
-        rank = Recommend(user, train, K, N)
+        rank = Recommend(user, train, W, K, N)
         for item, pui in rank.items():
             ret += math.log(1 + item_popularity[item])
             n_pop += 1
